53189829914e6bec45a9a5c40cf0416645448b62
Prateek Vishwakarma <prateekv2003@gmail.com>
2022-06-14 19:58:44 +0530

    cloudinery cloud img optimigation

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,89 +1,77 @@
 import React from 'react'
 import './portfolio.css'
-import Portfolio1 from '../../assests/portfolio-1.webp'
-import Portfolio2 from '../../assests/portfolio-2.webp'
-import Portfolio3 from '../../assests/portfolio-3.webp'
-import Portfolio4 from '../../assests/portfolio-4.webp'
-import Portfolio5 from '../../assests/portfolio-5.webp'
-import Portfolio6 from '../../assests/portfolio-6.webp'
-import Portfolio7 from '../../assests/portfolio-7.webp'
-import Portfolio8 from '../../assests/portfolio-8.webp'
-import Portfolio9 from '../../assests/portfolio-9.webp'
-import Portfolio10 from '../../assests/portfolio-10.webp'
-import Portfolio11 from '../../assests/portfolio-11.webp'
-import Portfolio12 from '../../assests/portfolio-12.webp'
 const Portfolio = () => {
     const data = [
         {
             id : 1,
-            image: Portfolio1,
+            image: "https://res.cloudinary.com/dww0uun9v/image/upload/c_scale,w_512/v1655209883/pixo/portfolio-1_frgear.webp",
             title: "Project Title",
             detailes: "#"
         },
         {
             id : 2,
-            image: Portfolio2,
+            image: "https://res.cloudinary.com/dww0uun9v/image/upload/c_scale,w_512/v1655209883/pixo/portfolio-2_fnpklf.webp",
             title: "Project Title",
             detailes: "#"
         },
 
         {
             id : 3,
-            image: Portfolio3,
+            image: "https://res.cloudinary.com/dww0uun9v/image/upload/c_scale,w_512/v1655209883/pixo/portfolio-3_byef7y.webp",
             title: "Project Title",
             detailes: "#"
         }, 
         {
             id : 4,
-            image: Portfolio4,
+            image: "https://res.cloudinary.com/dww0uun9v/image/upload/c_scale,w_512/v1655209883/pixo/portfolio-4_kkvbrc.webp",
             title: "Project Title",
             detailes: "#"
         },
         {
             id : 5,
-            image: Portfolio5,
+            image: "https://res.cloudinary.com/dww0uun9v/image/upload/c_scale,w_512/v1655209883/pixo/portfolio-5_anxi6c.webp",
             title: "Project Title",
             detailes: "#"
         },
         {
             id : 6,
-            image: Portfolio6,
+            image: "https://res.cloudinary.com/dww0uun9v/image/upload/c_scale,w_512/v1655209883/pixo/portfolio-6_bamk5w.webp",
             title: "Project Title",
             detailes: "#"
         },
         {
             id : 7,
-            image: Portfolio7,
+            image: "https://res.cloudinary.com/dww0uun9v/image/upload/c_scale,w_512/v1655209883/pixo/portfolio-7_ifqoml.webp",
             title: "Project Title",
             detailes: "#"
         },
         {
             id : 8,
-            image: Portfolio8,
+            image: "https://res.cloudinary.com/dww0uun9v/image/upload/c_scale,w_512/v1655209884/pixo/portfolio-8_sm5kc4.webp",
             title: "Project Title",
             detailes: "#"
         },
         {
             id : 9,
-            image: Portfolio9,
+            image: "https://res.cloudinary.com/dww0uun9v/image/upload/c_scale,w_512/v1655209883/pixo/portfolio-9_hgo4bh.webp",
             title: "Project Title",
             detailes: "#"
         },
         {
             id : 10,
-            image: Portfolio10,
+            image: "https://res.cloudinary.com/dww0uun9v/image/upload/c_scale,w_512/v1655209884/pixo/portfolio-10_dbqg50.webp",
             title: "Project Title",
             detailes: "#"
         },
         {
             id : 11,
-            image: Portfolio11,
+            image: "https://res.cloudinary.com/dww0uun9v/image/upload/c_scale,w_512/v1655209884/pixo/portfolio-11_fk6hlc.webp",
             title: "Project Title",
             detailes: "#"
         },
         {
             id : 12,
-            image: Portfolio12,
+            image: "https://res.cloudinary.com/dww0uun9v/image/upload/c_scale,w_512/v1655209884/pixo/portfolio-12_jgbtk2.webp",
             title: "Project Title",
             detailes: "#"
         },
@@ -91,12 +79,12 @@ const Portfolio = () => {
   return (
     <section className="portfolio">
         <h1>Some of our work</h1>
-
         {
             data.map(({id, image, title, detailes})=>{
                 return(
                     <figure key={id} className="port-item">
-                        <img src={image} alt="portfolio item"/>
+                        {/* <img src={image} alt="portfolio item"/> */}
+                        <img className="cld-responsive" src={image} alt="portfolio item"/>
                         <figcaption className="port-desc">
                             <p>{title}</p>
                             <a href={detailes} className="button button-accent button-small">Project details</a>
